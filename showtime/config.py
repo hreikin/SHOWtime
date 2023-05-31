1eb6f9ba7c89c78fbe11ad82e6fbf8caeda3755e
Michael Haslam <hreikin@gmail.com>
2023-05-31 11:35:00 +0100

    Fix imports.

--- showtime/config.py
+++ showtime/config.py
@@ -1,22 +1,8 @@
-from showtime.tabs.hello import HelloTab
-#from showtime.tabs.bitcoin import BitcoinPrice, Bitcoind
-from showtime.tabs.sysinfo import SystemStats, DiskUsage
-from showtime.tabs.uptime import WebsiteUptime
-
-import os
+from showtime.tabs import HelloTab, SystemStats, DiskUsage, WebsiteUptime
 
 # Add any tabs you want to be visible here
 tabs = [
          HelloTab(),
-
-         # Track a running Bitcoin node
-         #Bitcoind({"host": "http://127.0.0.1:8332",
-         #          "username": "bitcoinrpc",
-         #          # Read the password from a file
-         #          "password": "password" }),
-        
-         # A Bitcoin price ticker
-         #BitcoinPrice(),
          
          # Displays CPU, RAM usage and uptime
          SystemStats(),
